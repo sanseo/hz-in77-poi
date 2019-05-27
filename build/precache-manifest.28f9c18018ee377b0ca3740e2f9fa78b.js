9eb51c0a06b75434a5b898e1e29b9b424ea13d23
sensen <328966894@qq.com>
2019-05-27 21:50:08 +0800

    build

---
+++ build/precache-manifest.28f9c18018ee377b0ca3740e2f9fa78b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "421c255318ded16a6fe17b6a1851a646",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a28f16b9a2b635cf9e5",
+    "url": "/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "9a28f16b9a2b635cf9e5",
+    "url": "/static/js/main.094d6a15.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
